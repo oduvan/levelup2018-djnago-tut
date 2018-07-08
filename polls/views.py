94056c24ba8e3a1225074d9ae7e968e85eac0a1b
Alexander Lyabah <a.lyabah@checkio.org>
2018-07-08 15:15:55 +0300

    first commit

---
+++ polls/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from .models import Question
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    question = Question.objects.get(id=question_id)
+    return render(request, 'polls/index.html', {
+        'question': question
+        })
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
